02170f4d69ab42e37c20bd1912227c431ba1d7c3
Rajat Dixit <rd4november2000@gmail.com>
2026-02-20 03:31:45 +0530

    this commit adds redux store and front end to backend calls

---
+++ src/components/Login.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import Navbar from "./Navbar";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { Base_URL } from "../utils/constants";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("Nittu@gmail.com");
+  const [password, setPassword] = useState("Nittu@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        Base_URL,
+        {
+          emailId,
+          password,
+        },
+        {
+          withCredentials: true,
+        },
+      );
+      dispatch(addUser(res.data));
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+
+          <div>
+            <fieldset className="fieldset my-2">
+              <legend className="fieldset-legend">Email Address</legend>
+              <input
+                type="email"
+                value={emailId}
+                className="input input-bordered w-full mt-2 text-center"
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </fieldset>
+
+            <fieldset className="fieldset my-2">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="password"
+                value={password}
+                className="input input-bordered w-full mt-2 text-center"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+
+          <div className="card-actions justify-center m-2">
+            <button className="btn btn-secondary w-40" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const Navbar = () => {
+  const user = useSelector((store) => store.user);
+  console.log(user);
+  return (
+    <div>
+  <div className="navbar bg-base-200 shadow-sm">
+
+    <div className="flex-1">
+      <a className="btn btn-ghost text-xl">
+        ❤️ Dev Tinder - PairUp 
+      </a>
+    </div>
+
+    {user && (
+      <div className="flex items-center gap-4 mx-5">
+
+        <p className="font-medium">
+          Welcome {user.firstName}
+        </p>
+
+        <div className="dropdown dropdown-end">
+
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="profile"
+                src="https://c.saavncdn.com/346/Giga-Chad-English-2022-20230623063017-500x500.jpg"
+              />
+            </div>
+          </div>
+
+          <ul
+            tabIndex={-1}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
+          >
+            <li><a>Profile</a></li>
+            <li><a>Settings</a></li>
+            <li><a>Logout</a></li>
+          </ul>
+
+        </div>
+      </div>
+    )}
+
+  </div>
+</div>
+
+  );
+};
+
+export default Navbar;
